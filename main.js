f56b09c96e976884c687db621f077ab497c4e10a
“vvela2022” <victoriavela01@gmail.com>
2022-08-10 10:35:48 -0500

    added value to array and updated code to ref new values

--- main.js
+++ main.js
@@ -36,15 +36,28 @@ let warPDisp = document.querySelector('#player-war-display')
 const hTag = document.querySelector('h1')
 //populating master deck variable
 
-let cardTypes = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+let beginDeck = []
+let cardTypes = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 let suits = ['♥️', '♦️', '♣️', '♠️']//either replace w/ images or add image URLs to suits  
+let cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 let gameCondition = 'normal'
+
+function initialDeck() {
+    for(let i = 0; i < cardTypes.length; i++) {
+        beginDeck.push([cardValues[i],cardTypes[i]])
+    }
+    return beginDeck
+}
+
+initialDeck()
+
+
 //these lines created my master deck
 function createDeck() {
-    for(let i = 0; i < cardTypes.length; i++) {
+    for(let i = 0; i < beginDeck.length; i++) {
         for(let j = 0; j < suits.length; j++) {
-        masterDeck.push([cardTypes[i], suits[j]]) 
+        masterDeck.push([beginDeck[i], suits[j]]) 
         }
     } 
     return masterDeck
@@ -65,6 +78,7 @@ function deckShuffle(deck) {
 
 deckShuffle(masterDeck)
 
+//add additional value to the inner array at index 2
 
 //these lines will distributes 26 cards to each player
 function divyCards () {
@@ -74,6 +88,7 @@ function divyCards () {
 
 divyCards()
 
+console.log(player.deck)
 
 //this is where gameplay happens
 function gameTurn() {
@@ -81,17 +96,16 @@ function gameTurn() {
         playerDisplay.innerText = player.deck[0]
         computerDisplay.innerText = computer.deck[0]
         gameCondition = 'normal'
-        if(player.deck[0][0] > computer.deck[0][0]) {
+        if(player.deck[0][0][0] > computer.deck[0][0][0]) {
             let itemWon = computer.deck.splice(0, 1)
             player.deck.push(itemWon[0])//splice returns an array, so you need to reference the 0 index of the array
             let sendEnd = player.deck.splice(0, 1)
             player.deck.push(sendEnd[0])
-        } else if(computer.deck [0][0] === player.deck[0][0]) {
-            
+        } else if(computer.deck [0][0][0] === player.deck[0][0][0]) {
             initiateWar()
             setTimeout(clearWar, 2000)
             console.log('tie')
-            if(computer.deck[2][0] > player.deck[2][0]) {
+            if(computer.deck[2][0][0] > player.deck[2][0][0]) {
                 console.log('computer won hand')
                 
                 let warWin = player.deck.splice(0, 3)
@@ -102,10 +116,10 @@ function gameTurn() {
                 for (let j = 0; j < sendEnd.length; j++) {
                     computer.deck.push(sendEnd[j])
                 }
-            } else if (computer.deck[2][0] === player.deck[2][0]) {
+            } else if (computer.deck[2][0][0] === player.deck[2][0][0]) {
                 initiateWar()
                 setTimeout(clearWar, 2000)
-                if(computer.deck[4][0] > player.deck[4][0]) {
+                if(computer.deck[4][0][0] > player.deck[4][0][0]) {
                     console.log('computer won hand')
                     let warWin = player.deck.splice(0,5)
                     for(let i = 0; i < warWin.length; i++) {
@@ -152,27 +166,6 @@ function gameTurn() {
         console.log(`Player: ${player.cardCount}`)
         console.log(gameCondition)
         checkWin()
-        // if (player.cardCount === 0) {
-        //     //alert('Sorry, you lose!') 
-        //     hTag.innerText = 'Sorry, you lose!!'
-        //     // document.addEventListener('click', function () {
-        //     //    location.reload() 
-        //     // })
-        // } else if (computer.cardCount === 0) {
-        //     //alert('You win!!')
-        //     hTag.innerText = 'You win!!'
-            
-        //     // document.addEventListener('click', function() {
-        //     //     location.reload()
-        //     // })
-        // } else if(gameCondition ==='war' && player.cardCount < 3) {
-        //     hTag.innerText = 'Sorry, you don\'t have enough cards to continue, you lose.'
-        //     // alert('Sorry, you don\'t have enough cards to continue, you lose!')
-        // } else if(gameCondition === 'war' && computer.cardCount < 3) {
-        //     hTag.innerText = 'The computer does not have enough cards to continue, you win!!'
-        //     // alert('The computer does not have enough cards to continue, you win!!'
-        //     // )
-        // } 
     })
 }
   
@@ -202,13 +195,11 @@ function updateScore() {
 
 function checkWin() {
     if (player.cardCount === 0) {
-        //alert('Sorry, you lose!') 
         hTag.innerText = 'Sorry, you lose!!'
         // document.addEventListener('click', function () {
         //    location.reload() 
         // })
     } else if (computer.cardCount === 0) {
-        //alert('You win!!')
         hTag.innerText = 'You win!!'
         
         // document.addEventListener('click', function() {
